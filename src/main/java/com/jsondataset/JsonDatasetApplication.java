01f3a74eee1570454e98f50fb96962c79d8ea2bd
Abhishek Dumaniya <abhishekdumaniya196@gmail.com>
2025-08-21 11:35:46 +0530

    initial commit

---
+++ src/main/java/com/jsondataset/JsonDatasetApplication.java
@@ -0,0 +1,13 @@
+package com.jsondataset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonDatasetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonDatasetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsondataset/JsonDatasetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsondataset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonDatasetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
